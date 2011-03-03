2072f30f20f455ef586ded3ef87a06ff6401c0d3
Jason Jarrett <jason@elegantcode.com>
2011-03-03 09:23:15 -0800

    added extra assemblies for possible phone integration

--- src/StatLight.IntegrationTests/ProviderTests/TestXapFileLocations.cs
+++ src/StatLight.IntegrationTests/ProviderTests/TestXapFileLocations.cs
@@ -2,14 +2,16 @@
 
 namespace StatLight.IntegrationTests.ProviderTests
 {
-	public class TestXapFileLocations
-	{
-		private static readonly string CurrentDirectory = Environment.CurrentDirectory + @"\\ProviderTests\_TestXaps\";
+    public class TestXapFileLocations
+    {
+        private static readonly string CurrentDirectory = Environment.CurrentDirectory + @"\\ProviderTests\_TestXaps\";
 
-		public static string MSTest = CurrentDirectory + "StatLight.IntegrationTests.Silverlight.MSTest.xap";
-		public static string NUnit = CurrentDirectory + "StatLight.IntegrationTests.Silverlight.NUnit.xap";
-		public static string UnitDriven =  CurrentDirectory + "StatLight.IntegrationTests.Silverlight.UnitDriven.xap";
-		public static string XUnit = CurrentDirectory + "StatLight.IntegrationTests.Silverlight.Xunit.xap";
-		public static string SilverlightIntegrationTests = CurrentDirectory + "StatLight.IntegrationTests.Silverlight.xap";
-	}
+        public static string MSTest = CurrentDirectory + "StatLight.IntegrationTests.Silverlight.MSTest.xap";
+        public static string NUnit = CurrentDirectory + "StatLight.IntegrationTests.Silverlight.NUnit.xap";
+        public static string UnitDriven = CurrentDirectory + "StatLight.IntegrationTests.Silverlight.UnitDriven.xap";
+        public static string XUnit = CurrentDirectory + "StatLight.IntegrationTests.Silverlight.Xunit.xap";
+        public static string SilverlightIntegrationTests = CurrentDirectory + "StatLight.IntegrationTests.Silverlight.xap";
+        public static string WinPhone = CurrentDirectory + "StatLight.IntegrationTests.Phone.MSTest.xap";
+
+    }
 }
